9c491d7d879f7de96414e84c7b2611686db2a7df
Kanishka Hitihamillage <khitihamillage@simonschools.net>
2025-06-06 15:34:54 +1000

    Fix indentation in Vite server configuration

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
 	},
 	server: {
 		port: 3006,
-		 allowedHosts: 'all'
+		allowedHosts: 'all'
 	},
 	build: {
 		outDir: "dist", // Ensure the output directory is correct

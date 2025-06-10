2652cb2f126a5f0f3b582474b910f351040176ae
Kanishka1g1 <kanishkagunarathne@gmail.com>
2025-06-10 15:02:16 +1000

    revert

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,3 @@
-# FROM node:18-alpine
-
-# WORKDIR /app
-
-# COPY package*.json ./
-
-# RUN npm install
-
-# COPY . .
-
-# RUN npm run build
-
-
 FROM node:18-alpine
 
 WORKDIR /app
@@ -21,6 +8,4 @@ RUN npm install
 
 COPY . .
 
-EXPOSE 3006
-
-CMD ["npm", "run", "dev", "--", "--host", "--port", "3006"]
+RUN npm run build
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
+
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 import vuetify from "vite-plugin-vuetify";
 
+// https://vite.dev/config/
 export default defineConfig({
 	base: "/",
 	plugins: [vue(), vueDevTools(), vuetify({ autoImport: true })],
@@ -14,12 +16,12 @@ export default defineConfig({
 	},
 	server: {
 		port: 3006,
-		host: "0.0.0.0",
+		allowedHosts: ["korder.cloud"],
 	},
 	build: {
-		outDir: "dist",
-		assetsDir: "assets",
-		emptyOutDir: true,
+		outDir: "dist", // Ensure the output directory is correct
+		assetsDir: "assets", // Ensure static assets are placed in the correct directory
+		emptyOutDir: true, // Clear the output directory before building
 	},
 });
 

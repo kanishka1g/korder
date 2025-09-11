f400138420ee18b61206ab10d4d745a1f74db3ba
Kanishka Gunarathne <kanishkagunarathne@gmail.com>
2025-09-11 23:48:36 +1000

    remove redundant dotenv import statement from server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,7 +5,6 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.js";
 import habitRoutes from "./routes/habit.js";
 import { authMiddleware } from "./middleware/authMiddleware.js";
-import "dotenv/config";
 
 dotenv.config();
 

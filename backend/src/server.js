d59b63b927763df424e15346c8ddccf85f02a5cd
Kanishka Gunarathne <kanishkagunarathne@gmail.com>
2025-09-13 21:06:44 +1000

    update MongoDB connection string and improve server startup log message

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,9 @@ app.use(cors());
 app.use(express.json());
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(
+    "mongodb+srv://kanishka:Plesewait89@korder.lon8u.mongodb.net/korderDB?retryWrites=true&w=majority&appName=Korder"
+  )
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
@@ -29,6 +31,4 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>
-  console.log(`Server running on port ${process.env.MONGO_URI}`)
-);
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
